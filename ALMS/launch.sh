d2a896abf7e03d74cb3459064f16c2132f6312c0
zhangyx <yuxuan.zhang.1998@gmail.com>
2018-04-26 15:21:53 -0700

    Renamed V1.00 Release

--- ALMS/launch.sh
+++ ALMS/launch.sh
@@ -21,5 +21,3 @@ TS=$[${TS_SC}*1000+TS_MS/1000000]
 #echo $C_DIR'/'$AIO_path'_'$Version'.py' $CONFIG_AIO $TS $1
 #echo $C_DIR'/'$WPG_path'_'$Version'.py' $CONFIG_WPG $1
 python $C_DIR'/'$AIO_path $CONFIG_AIO $TS '--Path' $C_DIR'/' $1
-python $C_DIR'/'$WPG_path $CONFIG_WPG $TS '--Path' $C_DIR'/' $1
-
