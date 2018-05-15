5c604e76564a48f164c2fa7efca1358a3264bfa8
Yuxuan Zhang <zhangyx1998@inkfish.dhcp.lbl.gov>
2018-05-15 11:01:33 -0700

    V4.17 Release

--- ALMS/config.sh
+++ ALMS/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Author:Yuxuan Zhang
 
-Version="V4.16"
+Version="V4.17"
 
 #-----------------------------------------------------
 #------------------Global Arguments-------------------
