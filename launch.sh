fc7da51a4a6350afda6cda41026c184a8594825a
Yuxuan Zhang <zhangyx1998@inkfish.dhcp.lbl.gov>
2018-05-15 10:14:29 -0700

    V4.17 Release

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Author:YuxuanZhang
 C_DIR="$( cd "$( dirname "$0" )" && pwd )"
-CMD="${C_DIR}/Current_Version/launch.sh ${C_DIR} $1"
+CMD="${C_DIR}/ALMS/launch.sh ${C_DIR} $1"
 #echo $CMD
 $CMD
